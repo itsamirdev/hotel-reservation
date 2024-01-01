41c9a99725f583f5dcdd3e9f6378e32ef7f19a0a
itsamirdev <itsamirhosseindev@gmail.com>
2024-01-01 19:20:55 +0330

    create base structure

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # external apps
+    'rest_framework',
+    # internal apps
+    'hotel',
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+
